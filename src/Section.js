2afd102ed14ae918f5dfdea441373ff7ed9a1558
Jack Denson <jackdenson47@gmail.com>
2020-12-02 10:23:14 -0500

    Added mobile interface

--- src/Section.js
+++ src/Section.js
@@ -2,25 +2,41 @@ import React from 'react';
 import './Section.css'
 import { Paper } from '@material-ui/core'
 import FadeIn from './FadeIn.js'
+import Divider from '@material-ui/core/Divider';
+
 
 class Section extends React.Component {
 
-  state = {'height': '60vh'}
+  constructor(props) {
+    super(props);
+    this.state = {'height': '60vh', 'mobile':(window.innerWidth < 500)}
+  }
 
   componentDidMount() {
-    this.setState({'height': (this.refs.wrapper.clientHeight+'px')});
-    window.addEventListener('resize', () => {this.setState({'height': (this.refs.wrapper.clientHeight+'px')})})
+    this.updateComponentHeight();
+    window.addEventListener("resize", this.updateComponentHeight.bind(this));
   }
+
   componentWillUnmount() {
-    window.removeEventListener("resize", this.setState({'height': (this.refs.wrapper.clientHeight+'px')}))
+    window.removeEventListener("resize", this.updateComponentHeight.bind(this));
   }
+
+  updateComponentHeight() {
+    this.setState({'height': (this.refs.wrapper.clientHeight+'px'), 'mobile':(window.innerWidth < 500)})
+  }
+
 
   render() {
     let title = <Paper className ='title' elevation={5} style={{'height':this.state.height}}><div className="title-content"><h2>{this.props.title}</h2> <img src={this.props.image} className='icon' alt = {this.props.imAlt}/></div></Paper>
     let content = <Paper className = 'content-paper' elevation={5}><div className = "content">{this.props.children}</ div></Paper>
     let first = this.props.titleLeft ? title : content;
     let second = this.props.titleLeft ? content : title
-    return <FadeIn><div className="Section" ref='wrapper'>{first}{second}</div></FadeIn>
+    if(!this.state.mobile) {
+      return <FadeIn><div className="Section" ref='wrapper'>{first}{second}</div>{console.log(this.state.mobile)}</FadeIn>
+    }
+    else {
+      return <FadeIn><Paper className="Section-mobile" ref="wrapper"><h2>{this.props.title}</h2><img src={this.props.image} className='icon' alt = {this.props.imAlt}/><Divider /><div className='content-mobile'>{this.props.children}</div></Paper></FadeIn>
+    }
   }
 }
 export default Section;
